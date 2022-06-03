397d45b44f3907179b2c1c9364e9fb746c361924
MyGabriel <gabriellordmanu@gmail.com>
2022-06-03 05:40:09 +0000

    returning error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,14 @@
 /* Description: Pro without printf or puts to the std out. */
-#include<stdoi.h>
-#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 /**
-* main - 
+* main - is set 0
+*Return: 1
+*/
+
+int main(void)
+{
+	system("echo and that piece of art is use\" - Dora Kopar, 2015-10-19 \n");
+	return (1);
+}
