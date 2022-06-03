9a04006eb22eea011258188fd00a77bfda95a872
MyGabriel <gabriellordmanu@gmail.com>
2022-06-03 03:50:16 +0000

    Practicing C

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,14 @@
+/* Description: Using put for std out */
+#include <stdio.h>
+
+/**
+* main - void
+* Return: 0
+*/
+
+int main(void)   /* The starting point */
+
+{
+	puts("\"Programming is like building a multilingual puzzle.");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/* Description: Practicing printf to the std out */
+#include<stdio.h>
+
+/**
+* main - set void
+* Return: 0
+*/
+
+int main(void)
+{
+	printf("with proper grammer, burt the outcome is a piece of art,\n");
+	return (0);
+}
