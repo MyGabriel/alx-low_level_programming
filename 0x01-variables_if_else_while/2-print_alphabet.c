6e3fa8b7b3c5be6a1576bfa848cb2ea6744b5449
MyGabriel <gabriellordmanu@gmail.com>
2022-06-04 05:49:37 +0000

    A,B,C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+/* Description: Printing alphabets A-Z */
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - set to void
+* Return: 0
+* Using only putchar but not more than 2
+*/
+
+int main(void)
+{
+	char _alphabet;
+
+	_alphabet = 'a';
+	_alphabet == _alphabet++;
+		{
+			putchar(_alphabet);
+
+			while
+				(_alphabet <= 'z');
+		}
+	putchar('\n');
+	return (0);
+}
