abb948a4a87d1009a1ae92d8ec9fb26c5747b12d
MyGabriel <gabriellordmanu@gmail.com>
2022-06-06 02:30:19 +0000

    123 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+/* Description: Printing alphabets A-Z */
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - set to void
+* Return: 0
+* Using only putchar but not more than 2
+*/
+
+int main(void)
+{
+	int MyG = '0';
+
+	while
+		(MyG <= '9')
+	{
+		putchar(MyG);
+		MyG++;
+	}
+	putchar('\n');
+	return (0);
+}
