96e0bea6b57d49f2adbcccc7042a9c138644ebb3
MyGabriel <gabriellordmanu@gmail.com>
2022-06-08 02:36:44 +0000

    x10 alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+/* Description: Usint personal header file */
+#include "main.h"
+
+/**
+* print_alphabet_x10 - print alphabet of letters
+* Return: 0
+* Printing lower case abc
+*/
+
+void print_alphabet_x10(void)
+{
+	int My = 'a';
+	int My1 = 0;
+
+	while
+		(My1 < 10)
+	{
+		while
+			(My <= 'z')
+
+			_putchar(My);
+			My++;
+	}
+	My1++;
+	_putchar('\n');
+}
