e55a7c367e63d260f87b4293c12e0046837b9891
MyGabriel <gabriellordmanu@gmail.com>
2022-06-08 01:20:24 +0000

    alpha abc

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+/* Description: Usint personal header file */
+#include "main.h"
+
+/**
+* main - set to void
+* Return: 0
+* Printing lower case abc
+*/
+
+int main(void)
+{
+	int e = 'a';
+
+	while
+		(e <= z)
+	{
+		_putchar(e);
+		e++;
+	}
+	_putchar('\n');
+	return (0);
+}
