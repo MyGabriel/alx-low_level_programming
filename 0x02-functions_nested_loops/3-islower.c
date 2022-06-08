8911a16b3f66c851b48b04f8f776d14ea4b24a56
MyGabriel <gabriellordmanu@gmail.com>
2022-06-08 03:19:27 +0000

    check abc

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+/* Description: A pro to check abc */
+#include "main.h"
+
+/**
+ * _islower - prints 1 or 0 depending on input
+ * @c: first parameter
+ *
+ * Return: Always(0).
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
