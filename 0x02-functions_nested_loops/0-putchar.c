81438569ad6d0e83a98c2610cb3442aa2e655423
MyGabriel <gabriellordmanu@gmail.com>
2022-06-08 01:05:56 +0000

    coding with created header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+/* Description: Usint personal header file */
+#include "main.h"
+
+/**
+* main - set to void
+* Return: 0
+*/
+
+int main(void)
+{
+	int e = 0;
+	char f[] = "_putchar";
+
+	while
+		(e <= 8)
+	{
+		_putchar(f[e]);
+		e++;
+	}
+	_putchar('\n');
+	return (0);
+}
