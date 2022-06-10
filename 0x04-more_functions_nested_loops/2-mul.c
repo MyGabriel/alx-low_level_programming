aa809f7aca687d833bf57e1a858dd6198be70ec8
MyGabriel <gabriellordmanu@gmail.com>
2022-06-10 02:06:20 +0000

    Committing more func and loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+/* File2: A func to multiply two int */
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: multiplying
+ * @b: multiplier
+ * Return: the result of mutiplication
+ */
+
+int mul(int a, int b)
+{
+	int res_t;
+
+	res_t = a * b;
+	return (res_t);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+More func and loops tasks
