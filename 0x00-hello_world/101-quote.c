d97d4593a0d63747191521b0aa3205d7b1f283df
MyGabriel <gabriellordmanu@gmail.com>
2022-06-19 11:29:08 +0000

    Updating 0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 /* Description: Pro without printf or puts to the std out. */
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
-* main - is set 0
-*Return: 1
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -", 34);
-	write(2, " Dora Korpar, 2015-10-19\r\n", 25);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
