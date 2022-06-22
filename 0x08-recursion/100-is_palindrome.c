9db904bbc0ab08bff835cbcce2c8e2cdd974d055
MyGabriel <gabriellordmanu@gmail.com>
2022-06-22 15:37:30 +0000

    Task 0x08 bonu

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,21 @@
+/* File7: A func to ret 1 if a string is a palindrome or 0 if not */
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_palindrome - find a palindrome string
+ * @s: string for check
+ *
+ * Return: 1 if is palindrome, otherwise 0
+ */
+
+int is_palindrome(char *s)
+{
+	int i;
+
+	i = 0;
+
+	if (*s == i)
+		return (0);
+	return (1);
+}
