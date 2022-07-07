dd19bcbf0c18c1078df77976628ac84c519f61c3
MyGabriel <gabriellordmanu@gmail.com>
2022-07-07 00:39:08 +0000

    Func pointer in C

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,27 +1,41 @@
 /* File100: a func that prints it's own opcodes */
-#include "function_pointers.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - prints the opcodes
- * @argc: argument counter
- * @argv: argument vector
+ * main - entry point for program
+ * @argc: arg counter, number of args
+ * @argv: array of pointers to args, in order given
  *
- * Return: Always 0 (Sucess)
+ * Return: 0 on success, 1 if argc not right, 2 if bytes is negative
  */
 
 int main(int argc, char *argv[])
 {
+	int (*f)(int, char**);
+	int i = 0;
+	int hexByte;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	if (atoi(argv[1]) < 0)
+	else if (atoi(argv[1]) < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+	f = main;
+	while
+		(i < atoi(argv[1]))
+	{
+		hexByte = *(unsigned char *)(f + i);
+		printf("%.2x", hexByte);
+		if (i < atoi(argv[1]) - 1)
+			putchar(' ');
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
